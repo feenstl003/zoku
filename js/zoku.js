6f32b7c2d2c11491a56ff42a37a620c8651fcf44
Elton <elton@041.222.028.145.hva.nl>
2017-01-16 11:17:18 +0100

    added popup

--- js/zoku.js
+++ js/zoku.js
@@ -23,11 +23,34 @@ navOpen.addEventListener("click", function(){
   sideMenu.classList.add("open")
 })
 
-joinButton.addEventListener("click", ()=>{
-  // confirm dialog
-  alertify.confirm("Message", function () {
+if (joinButton) {
+  joinButton.addEventListener("click", ()=>{
+    // confirm dialog
+    alertify.confirm("Message", function () {
       alertify.success("You've clicked OK and typed")
-  }, function() {
+    }, function() {
       alertify.error("You've clicked Cancel and typed")
-  });
-} )
+    });
+  } )
+}
+
+if (!joinButton) {
+  setTimeout(()=>{
+
+    var msg = `
+    <div>
+      <img style="height: 15em" src="/images/coding.jpg"/>
+      <p>Inspired by all these workshops? Why not create your own?</p>
+      <ol>
+        <li>Go to the digital screen in the Living Room at Zoku.</li>
+        <li>Tap on the workshop you like the most and click to <strong>join</strong>.</li>
+        <li>Last but not least, think about your talents. Maybe someday others can <strong>join</strong> you in a workshop you create yourself.</li>
+      </ol>
+    </div>`
+
+
+    alertify
+      .okBtn("CREATE A WORKSHOP")
+      .alert(msg)
+  }, 10000)
+}
